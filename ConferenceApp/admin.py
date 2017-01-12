a769e2397749afc0a8f0c615bb68f6607fabddfa
agata.skierska <ag.skierska@gmail.com>
2017-01-12 17:38:10 +0100

    added features to the admin-page

--- ConferenceApp/admin.py
+++ ConferenceApp/admin.py
@@ -2,18 +2,44 @@ from django.contrib import admin
 from .models import Speaker, Session, Track
 
 
-# Register your models here.
 class SpeakerAdmin(admin.ModelAdmin):
+
     list_display = ('name', 'bio')
+    fieldsets = (
+        ("General Information", {"fields": ("name", "bio",)
+        }),
+        ("Social media", {
+            "classes": ("collapse",),
+            "fields": ("fb", "twitter",)
+        })
+    )
 
 
 admin.site.register(Speaker, SpeakerAdmin)
 
 
 class SessionAdmin(admin.ModelAdmin):
+
+    def status_approved(self, request, queryset):
+        rows_updated = queryset.update(status='a')
+        success_message = '{} session(s) updated'.format(rows_updated)
+        self.message_user(request, success_message)
+
+    status_approved.short_description = "Mark as approved"
+
+    def status_rejected(self, request, queryset):
+        rows_updated = queryset.update(status='r')
+        success_message = '{} session(s) updated'.format(rows_updated)
+        self.message_user(request, success_message)
+
+    status_rejected.short_description = "Mark as rejected"
+
     search_fields = ['title', 'abstract']
-    list_filter = ('track__title', 'speaker',)
-    list_display = ('title', 'track', 'speaker',)
+    list_filter = ('track__title', 'speaker', 'status')
+    list_display = ('title', 'speaker', 'status')
+    actions = [status_approved, status_rejected]
+
+
 
 
 admin.site.register(Session, SessionAdmin)
--- ConferenceApp/models.py
+++ ConferenceApp/models.py
@@ -6,7 +6,9 @@ class Speaker(models.Model):
     name = models.CharField(max_length=60)
     title = models.CharField(max_length=60)
     bio = models.TextField(max_length=500)
+    twitter = models.CharField(max_length=60, blank=True, null=True)
     fb = models.CharField(max_length=60, blank=True, null=True)
+
 
     def __str__(self):
         return self.name
@@ -20,11 +22,19 @@ class Track(models.Model):
         return self.title
 
 
+SESSION_STATUS = (
+    ('a', 'Approved'),
+    ('p', 'pending'),
+    ('r', 'rejected'),
+
+)
+
 class Session(models.Model):
     title = models.CharField(max_length=80)
     abstract = models.TextField(max_length=500)
     track = models.ForeignKey(Track)
     speaker = models.ForeignKey(Speaker)
+    status = models.CharField(max_length=1, choices=SESSION_STATUS, default='p')
 
     def __str__(self):
         return self.title
