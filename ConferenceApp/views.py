1f51d2ba391ff459f8dbd4d15664e196366bdc77
agata.skierska <agata.skierska@jmr.pl>
2017-01-12 18:05:23 +0100

    home-page

--- ConferenceApp/views.py
+++ ConferenceApp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 
 
 # Create your views here.
-
+def index(request):
+    return render(request, 'ConferenceApp/index.html', {})
